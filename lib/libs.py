acf1d111fb3dd5173de843eb37e204524b67d1eb
RafsanMazumder <rafsan.maz@gmail.com>
2020-07-23 11:00:42 +0600

    Add notes

--- lib/libs.py
+++ lib/libs.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 
 
 def windowed_dataset(series, window_size, batch_size, shuffle_buffer):
+    """prepare a time series for machine learning by windowing the data"""
     dataset = tf.data.Dataset.from_tensor_slices(series)
     dataset = dataset.window(window_size + 1, shift=1, drop_remainder=True)
     dataset = dataset.flat_map(lambda window: window.batch(window_size + 1))
@@ -11,6 +12,7 @@ def windowed_dataset(series, window_size, batch_size, shuffle_buffer):
 
 
 def split_train_valid(split_time, series, time):
+    """split the data series into training and validation portions"""
     x_train = series[:split_time]
     x_valid = series[split_time:]
     time_train = time[:split_time]
